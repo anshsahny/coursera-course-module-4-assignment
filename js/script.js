85b9437aa11b17480cf6115f3273f445a9740293
Ansh Sahny <anshsahny@gmail.com>
2020-09-04 20:09:21 -0400

    Module 4 Assignment Solution

--- js/script.js
+++ js/script.js
@@ -3,9 +3,8 @@
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
 
-  if (firstLetter === 'j') {
+  if (names[i].charAt(0) === 'J') {
     byeSpeaker(names[i]);
   }
   else {
